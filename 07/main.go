f638c3493fd101dfc940c17c6cd595b127702ceb
codecoco <88923210+codecocos@users.noreply.github.com>
2021-11-30 22:52:41 +0900

    if , switch

---
+++ 07/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func canIDrink(age int) bool{
+	//variable expression
+	if koreanAge := age +2; koreanAge < 18{
+		return false
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(canIDrink(16))
+}
